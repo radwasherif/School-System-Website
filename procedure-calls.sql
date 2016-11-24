c8cbc01fb99207533dc967c88e227e895949659e
radwasherif <radwasherif3@gmail.com>
2016-11-24 12:57:01 +0200

    Update procedure-calls.sql

--- procedure-calls.sql
+++ procedure-calls.sql
@@ -1,63 +1,45 @@
-CALL create_school ('Futures Language School', 'Maadi', '123456', 'provide quality education for leaders of the future', 'create a better future', 'English', 'Founded in 1999', 3000, 'national', 'futures@schools.com'); 
 
-CALL create_school ('El Bashayer', 'Maadi', '123457', 'provide quality education for leaders of the future', 'create a better future', 'English', 'Founded in 1998', 4000, 'national', 'bashayer@schools.com'); 
-
-CALL create_school ('Narmer', 'Maadi', '455662', 'educate through engaging', 'inspire creativity', 'English', 'offers scholarships', 6000, 'national', 'narmer@schools.com');
-
-CALL create_school ('Dar El Tarbeya', 'Mohandeseen', '123098', 'elite education according to international standards', 'educate and inspire', 'English', 'Founded in 1980', 30000, 'international', 'dareltarbeya@schools.com');
-
-CALL create_school ('Manor House', 'Dokki', '543266', 'provide quality education for leaders of the future', 'create a better future', 'English', 'Founded in 1983', 35000, 'international', 'manorhouse@schools.com'); 
-
-CALL create_school ('Lycee Francais', 'Downtown', '123886', 'perfect education', 'create a better future', 'French', 'Oldest French school in Egypt', 40000, 'national', 'lycee@schools.com');  
-
-CALL create_school ('El-Manar Experimental', 'Nasr City', '999999', 'Excellent quality education', 'Educate pioneers.', 'English', 'Founded in 1999', 2000, 'national', 'elmanar@schools.com');
-
-CALL create_school ('Hogwarts', 'Highlands of Scotland', '888888', 'Offers courses in witchraft and wizardry', 'Raising the wizardry masters of the future', 'English', 'Founded in 1999', 3000, 'national', 'hogwarts@schools.com');
-
-CALL create_school ('Victoria', 'Maadi', '111111', 'provide quality education for leaders of the future', 'create a better future', 'English', 'Founded in 1998', 4000, 'national', 'bashayer@schools.com'); 
+-- CALL create_school ('Victoria', 'Maadi', '111111', 'provide quality education for leaders of the future', 'create a better future', 'English', 'Founded in 1998', 4000, 'national', 'bashayer@schools.com'); 
 
 
-CALL add_school_level(1, 'elementary'); 
-CALL add_school_level(1, 'middle'); 
-CALL add_school_level(1, 'high');
-
-CALL add_school_level(2, 'elementary');
-CALL add_school_level(2, 'middle');
-
-CALL add_school_level(3, 'elementary');
-CALL add_school_level(3, 'middle');
-CALL add_school_level(3, 'high');
-
-CALL add_school_level(4, 'elementary');
-CALL add_school_level(4, 'middle');
-CALL add_school_level(4, 'high');
-
-CALL add_school_level(6, 'elementary');
-CALL add_school_level(6, 'middle');
-CALL add_school_level(6, 'high');
+-- CALL add_school_level(LAST_INSERT_ID(), 'elementary'); 
+-- CALL add_school_level(LAST_INSERT_ID(), 'middle'); 
+-- CALL add_school_level(LAST_INSERT_ID(), 'high');
 
 
-CALL add_school_level(7, 'elementary');
-CALL add_school_level(7, 'middle');
-CALL add_school_level(7, 'high');
+-- CALL create_course (204, 'Basic French II', '', 2, 'elementary');
+-- CALL create_course (304, 'Basic French III', '', 3, 'elementary');
+
+-- CALL add_course_prerequisite(204, 304);  
+
+-- CALL create_admin('Ahmad', 'Fathy', 'Elsayed', 'ahmad.elsayed', '12345yy', 'elsayed@gmail.com', 'male', '12 Khalid st, Tagamoa, Cairo, Egypt', '1978-02-03'); 
+
+-- CALL add_admin_to_school(LAST_INSERT_ID(), 'Victoria'); 
+
+-- CALL search_school('Victoria', 'International', 'Maadi'); 
+
+-- CALL user_view_school_info(1);
+
+-- CALL view_school_by_level();
+
+-- CALL teacher_sign_up ('Ismail', '', 'Yasseen', '1969-01-01', '21 Dokki st, Cairo, Egypt', 'som3a@gmail.com', 'male', 1); 
+-- CALL teacher_sign_up ('Foad', '', 'El-Mohandess', '1978-01-03', '34 El-Nahda st, Cairo, Egypt', 'foad@gmail.com', 'male', 1); 
+-- CALL teacher_sign_up ('Samira', '', 'Saeed', '1969-1-1', '21 Bohooth st, Cairo, Egypt', 'yomwarayom@gmail.com', 'female', 1); 
+-- CALL teacher_sign_up ('Amina', '', 'Rezk', '1940-04-1', '12 Dokki st, Cairo, Egypt', 'aminarezk@gmail.com', 'female', 2); 
+-- CALL teacher_sign_up ('Mona', '', 'Zaki', '1978-5-6', '21 Nile st, Cairo, Egypt', 'som3a@gmail.com', 'female', 2); 
 
 
-CALL add_school_level(8, 'elementary');
-CALL add_school_level(8, 'middle');
-CALL add_school_level(8, 'high');
+-- CALL view_applied_teacher(10); 
+-- CALL view_applied_teacher(11); 
+-- CALL view_applied_teacher(7); 
 
+-- CALL verify_applied_teacher(10, 4, 1); 
+-- CALL verify_applied_teacher(10, 3, 1); 
 
+-- CALL view_applied_students(10); 
+-- CALL view_applied_students(11); 
 
-CALL add_school_level(9, 'elementary');
-CALL add_school_level(9, 'middle');
-CALL add_school_level(9, 'high');
+-- CALL admin_accept_application(10, 612065); 
+-- CALL admin_accept_application(11, 623019);
 
-CALL create_course (101, 'Basic Arabic I', 'Basic rules of Arabic grammar', 1, 'elementary' );
-CALL create_course (201, 'Basic Arabic II', 'Rules of Arabic grammar', 2, 'elementary' );
-CALL create_course (301, 'Basic Arabic III', 'Rules of Arabic grammar', 3, 'elementary' );
-CALL create_course (401, 'Elementary Arabic I', 'Medium-level introductory Arabic course', 4, 'elementary' );
-CALL create_course (501, 'Basic Arabic I', 'Basic rules of Arabic grammar', 1, 'elementary' );
-CALL create_course (601, 'Basic Arabic I', 'Basic rules of Arabic grammar', 1, 'elementary' );
-CALL create_course (701, 'Basic Arabic I', 'Basic rules of Arabic grammar', 1, 'elementary' );
-
-   
+-- CALL parent_view_accepted(6); 
